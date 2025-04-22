622b333b71cf72841e0d912f6aff08667dee64fb
Pewiz <dvilla20152025@gmail.com>
2025-04-22 18:04:09 -0400

    Solucion del problema 4

--- 1_operadores/salarioConComisionPorVenta.c
+++ 1_operadores/salarioConComisionPorVenta.c
@@ -3,3 +3,22 @@
 // valor de la venta por auto. Cada mes el dueño de la empresa ingresa en la
 // computadora los datos pertinentes. Hacer un programa que calcule e imprima el
 // salario mensual de un vendedor dado.
+
+#include <stdio.h>
+
+int main()
+{
+    int salarioBase = 1000, cantiadadAutosVendidos, valorAuto;
+    float salarioTotal;
+    printf("\t--Salario con comision-- \n");
+    printf("Cantidad de autos vendidos: ");
+    scanf("%d", &cantiadadAutosVendidos);
+    printf("Valor del auto:");
+    scanf("%d", &valorAuto);
+
+    salarioTotal = salarioBase + (cantiadadAutosVendidos * 150) + ((valorAuto * 0.05) * cantiadadAutosVendidos);
+
+    printf("\nEl salario total es: %g", salarioTotal);
+
+    return 0;
+}
