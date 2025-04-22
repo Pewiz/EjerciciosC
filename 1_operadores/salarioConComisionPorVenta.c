623f6b7197697a2c19a1cec1a219e42a1d1e6097
Pewiz <dvilla20152025@gmail.com>
2025-04-22 17:47:52 -0400

    Problema 4

---
+++ 1_operadores/salarioConComisionPorVenta.c
@@ -0,0 +1,5 @@
+// 4. Una compañía de venta de autos usados, paga a su personal de ventas un salario de
+// $1000 mensuales, más una comisión de $150 por cada auto vendido, más el 5% del
+// valor de la venta por auto. Cada mes el dueño de la empresa ingresa en la
+// computadora los datos pertinentes. Hacer un programa que calcule e imprima el
+// salario mensual de un vendedor dado.
