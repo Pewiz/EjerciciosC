ca8f5e9272e95203abbdc1450fb9676260ae4115
Pewiz <dvilla20152025@gmail.com>
2025-04-22 17:25:42 -0400

    Problema 3

---
+++ 1_operadores/distribucionDeDinero.c
@@ -0,0 +1,3 @@
+// 3. Diego tiene N dólares. Felipe tiene la mitad de lo que posee Diego. Sebastián tiene
+// la mitad de lo que poseen Felipe y Diego juntos. Hacer un programa que calcule e
+// imprima la cantidad de dinero que tienen entre los tres.
