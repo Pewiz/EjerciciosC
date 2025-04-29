396b08ac4d2086b682422dadf60fef5faa80352e
Pewiz <dvilla20152025@gmail.com>
2025-04-29 17:07:15 -0400

    Solucion ejercicio 2

--- 2_condicionales/comparar2Numeros.c
+++ 2_condicionales/comparar2Numeros.c
@@ -1,1 +1,29 @@
 // 2. Pedir dos números y decir cual es mayor o si son iguales.
+
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2;
+
+    printf("Ingrese 2 numeros \n");
+    printf("Numero 1: ");
+    scanf("%d", &n1);
+    printf("Numero 2: ");
+    scanf("%d", &n2);
+
+    if (n1 == n2)
+    {
+        printf("Los numeros %d y %d son iguales", n1, n2);
+    }
+    else if (n1 > n2)
+    {
+        printf("El numero mayor es: %d", n1);
+    }
+    else
+    {
+        printf("El numero mayor es: %d", n2);
+    }
+
+    return 0;
+}
