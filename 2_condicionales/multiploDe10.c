15d652f01ae1499339235e106ec6390d75ef3af8
Pewiz <dvilla20152025@gmail.com>
2025-04-29 16:48:43 -0400

    Solucion problema 1

--- 2_condicionales/multiploDe10.c
+++ 2_condicionales/multiploDe10.c
@@ -1,2 +1,24 @@
 // 1. Hacer un programa que lea un número entero y muestre si el número es múltiplo
 // de 10.
+
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+
+    printf("\t Verificar multiplo de 10 \n");
+    printf("Ingrese un numero: ");
+    scanf("%d", &numero);
+
+    if (numero % 10 == 0)
+    {
+        printf("El numero %d es multiplo de 10", numero);
+    }
+    else
+    {
+        printf("El numero %d NO es multiplo de 10", numero);
+    }
+
+    return 0;
+}
