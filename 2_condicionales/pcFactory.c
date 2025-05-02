d913e8e280f206e9414fc5a1317c6d0ca3437886
Pewiz <dvilla20152025@gmail.com>
2025-05-01 23:49:52 -0400

    Solucion ejercicio 4

--- 2_condicionales/pcFactory.c
+++ 2_condicionales/pcFactory.c
@@ -1,2 +1,28 @@
 // 4. En PcFactory se hace un 20% de descuento a los clientes cuya compra supere los
 // $300. ¿Cuál será la cantidad que pagará una persona por su compra?
+
+#include <stdio.h>
+
+int main()
+{
+
+    int precio;
+    float descuento = 0.2, precioFinal;
+
+    printf("Ingrese el valor de la venta \n");
+    printf("$:");
+    scanf("%d", &precio);
+
+    if (precio > 300)
+    {
+        descuento *= precio;
+        precioFinal = precio - descuento;
+        printf("El precio final es: %f", precioFinal);
+    }
+    else
+    {
+        printf("En tu compra no hay descuento, el valor es: %d", precio);
+    }
+
+    return 0;
+}
