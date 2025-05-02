4d7ba6f1e4e188c81801b4077346612c637d0e42
Pewiz <dvilla20152025@gmail.com>
2025-05-01 23:51:56 -0400

    Solucion ejercicio 5

--- 2_condicionales/calculoSalario.c
+++ 2_condicionales/calculoSalario.c
@@ -3,3 +3,26 @@
 //  -Si trabaja 40 horas o menos se le paga $16 por hora.
 //  -Si trabaja más de 40 horas se le paga $16 por cada una de las primeras 40
 // horas y $20 por cada hora extra.
+
+#include <stdio.h>
+
+int main()
+{
+    int horasTrabajas, salarioSemanal, horasExtras;
+
+    printf("Ingrese las horas trabajas: ");
+    scanf("%d", &horasTrabajas);
+
+    if (horasTrabajas <= 40)
+    {
+        salarioSemanal = horasTrabajas * 16;
+        printf("Tu salario por trabajar %d es de %d", horasTrabajas, salarioSemanal);
+    }
+    else
+    {
+        horasExtras = (horasTrabajas - 40);
+        salarioSemanal = (horasTrabajas - horasExtras) * 16 + horasExtras * 20;
+        printf("Tu salario por trabajar %d es de %d", horasTrabajas, salarioSemanal);
+    }
+    return 0;
+}
