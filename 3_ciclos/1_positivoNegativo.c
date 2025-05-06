997a26f6cadd733b6c8e0318b62a6feb31759683
Pewiz <dvilla20152025@gmail.com>
2025-05-06 12:19:43 -0400

    Solucion ejercicio 1

--- 3_ciclos/1_positivoNegativo.c
+++ 3_ciclos/1_positivoNegativo.c
@@ -1,2 +1,29 @@
 // 1. Leer un número e indicar si es positivo o negativo. El proceso se repetirá hasta que
 // se introduzca un 0.
+
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+
+    do
+    {
+        printf("Ingresa un numero:");
+        scanf("%d", &numero);
+
+        if (numero != 0)
+        {
+            if (numero > 0)
+            {
+                printf("El numero es positivo\n");
+            }
+            else
+            {
+                printf("El numero es negativo\n");
+            }
+        }
+    } while (numero != 0);
+
+    return 0;
+}
