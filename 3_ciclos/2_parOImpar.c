674149591468c5b68ba560d40a300ca596f76003
Pewiz <dvilla20152025@gmail.com>
2025-05-06 12:52:54 -0400

    Ejercicio 2

--- 3_ciclos/2_parOImpar.c
+++ 3_ciclos/2_parOImpar.c
@@ -1,2 +1,2 @@
-// 3. Leer un número e indicar si es positivo o negativo. El proceso se repetirá hasta que
+// 2. Leer un número e indicar si es positivo o negativo. El proceso se repetirá hasta que
 // se introduzca un 0. Para cada uno indicar si es par o impar.
