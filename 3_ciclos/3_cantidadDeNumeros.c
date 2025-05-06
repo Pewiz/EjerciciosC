2b6053a2b8cb737f6c5dde8f61a88fd90d841e79
Pewiz <dvilla20152025@gmail.com>
2025-05-06 13:03:04 -0400

    Solucion ejercicio 3

--- 3_ciclos/3_cantidadDeNumeros.c
+++ 3_ciclos/3_cantidadDeNumeros.c
@@ -1,2 +1,22 @@
 // 3. Pedir números hasta que se introduzca uno negativo, y mostrar cuántos números se
 // han introducido.
+
+#include <stdio.h>
+
+int main()
+{
+    int numero, contador = 0;
+
+    printf("Ingresa un numero: ");
+    scanf("%d", &numero);
+
+    while (numero >= 0)
+    {
+        contador++;
+        printf("Ingresa un numero: ");
+        scanf("%d", &numero);
+    }
+
+    printf("La cantidad de numeros ingresados es: %d", contador);
+    return 0;
+}
