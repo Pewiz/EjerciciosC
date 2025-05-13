437470e4163d25f1294667e2be470ba6ae9b007a
Pewiz <dvilla20152025@gmail.com>
2025-05-13 17:11:18 -0400

    Solucion ejercicio 15

--- 3_ciclos/15_tablaDeMultiplicar.c
+++ 3_ciclos/15_tablaDeMultiplicar.c
@@ -1,1 +1,18 @@
 // 15. Realiza las tablas de multiplicar del 1 al 10
+
+#include <stdio.h>
+
+int main()
+{
+
+    for (int i = 1; i < 11; i++)
+    {
+        for (int j = 1; j < 11; j++)
+        {
+            printf(" %d * %d = %d\n", i, j, (i * j));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
