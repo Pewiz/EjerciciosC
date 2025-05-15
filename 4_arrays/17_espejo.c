ed7f9d13b12cb6b8c8dd7921efcb9468d19d2fba
David Osorio <91290489+Pewiz@users.noreply.github.com>
2025-05-14 20:27:47 -0400

    Ejercicio para practicar en casa

---
+++ 4_arrays/17_espejo.c
@@ -0,0 +1,6 @@
+// 17. Un vector se considera un “arreglo de espejo” si sus números que contiene hasta la mitad están repetidos en la otra mitad pero de manera inversa
+//
+// Ejemplos:
+//
+//         PAR                                    IMPAR
+//  [5][3][4][4][3][5]              [7][8][2][1][6][5][6][1][2][8][7]
