188483a8de77e1e241248a85936b1db247c7145a
Pewiz <dvilla20152025@gmail.com>
2025-05-27 17:13:00 -0400

    Solucion ejercicio 17

--- 4_arrays/17_espejo.c
+++ 4_arrays/17_espejo.c
@@ -4,3 +4,76 @@
 //
 //         PAR                                    IMPAR
 //  [5][3][4][4][3][5]              [7][8][2][1][6][5][6][1][2][8][7]
+
+#include <stdio.h>
+
+int main()
+{
+    int sizeArray, centro, bandera = 0;
+
+    printf("Ingresa el tamaño del arreglo: ");
+    scanf("%d", &sizeArray);
+
+    int numero[sizeArray];
+
+    // Llenado de arreglo
+    for (int i = 0; i < sizeArray; i++)
+    {
+        printf("Numero [%d]: ", i);
+        scanf("%d", &numero[i]);
+    }
+
+    centro = sizeArray / 2;
+
+    if (sizeArray % 2 == 0)
+    {
+        int a = centro - 1;
+        int b = centro;
+        while (a >= 0 && b < sizeArray)
+        {
+
+            if (numero[a] == numero[b])
+            {
+                bandera = 1; // Los valores de estas posiciones son iguales
+            }
+            else
+            {
+                bandera = 0; // False
+            }
+
+            a--;
+            b++;
+        }
+        printf("%d", bandera);
+    }
+    else
+    {
+        int a = centro - 1;
+        int b = centro + 1;
+        while (a >= 0 && b < sizeArray)
+        {
+
+            if (numero[a] == numero[b])
+            {
+                bandera = 1; // True
+            }
+            else
+            {
+                bandera = 0; // False
+            }
+            a--;
+            b++;
+        }
+    }
+
+    if (bandera == 1)
+    {
+        printf("El arreglo ingresado ES espejo");
+    }
+    else
+    {
+        printf("El arreglo ingresado NO espejo");
+    }
+
+    return 0;
+}
