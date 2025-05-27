3e1534d390c34605f48f90e7c9d47d0a5111357f
Pewiz <dvilla20152025@gmail.com>
2025-05-27 17:30:34 -0400

    Solucion ejercicio 7

--- 5_Matrices/7_marco.c
+++ 5_Matrices/7_marco.c
@@ -1,2 +1,28 @@
 // 7. Crear una matriz “marco” de tamaño 5x5: todos sus elementos deben ser 0 salvo
 // los de los bordes que deben ser 1. Mostrarla.
+
+#include <stdio.h>
+
+int main(){
+    int matriz[5][5];
+
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            if(i == 0 || i==4 || j==0 || j==4){
+                matriz[i][j] = 1;
+            }else{
+                matriz[i][j] = 0;
+            }
+        }
+    }
+
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
