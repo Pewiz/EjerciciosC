52a2562c1ef72cc5ec86c9e45477693a89909552
Pewiz <dvilla20152025@gmail.com>
2025-06-03 17:54:17 -0400

    Solucion ejercicio 8

--- 5_Matrices/8_filaConMayorSuma.c
+++ 5_Matrices/8_filaConMayorSuma.c
@@ -1,1 +1,39 @@
 // Encontrar la fila con mayor suma en una matriz: Pedir al usuario una matriz n x m de enteros, y mostrar cuál fila tiene la mayor suma de elementos.
+
+#include <stdio.h>
+
+int main()
+{
+
+    int matrix[5][5];
+    printf("Ingrese valores a la matriz:");
+
+    // Llenar un matriz
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            printf("\n[%d] x [%d]:", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    int sumaMayor = 0, filaMayor;
+    for (int i = 0; i < 5; i++)
+    {
+        int suma = 0;
+
+        for (int j = 0; j < 5; j++)
+        {
+            suma += matrix[i][j];
+        }
+        if (suma > sumaMayor)
+        {
+            sumaMayor = suma;
+            filaMayor = i;
+        }
+    }
+
+    printf("La fila mayor es: %d", filaMayor);
+
+    return 0;
+}
