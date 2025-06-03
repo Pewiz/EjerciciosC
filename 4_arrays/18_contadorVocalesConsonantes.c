9a2d2ca13c36a2541b5b60e5295f81c653bb3e1f
Pewiz <dvilla20152025@gmail.com>
2025-06-03 17:14:51 -0400

    Solucion ejercicio 18

--- 4_arrays/18_contadorVocalesConsonantes.c
+++ 4_arrays/18_contadorVocalesConsonantes.c
@@ -1,1 +1,38 @@
 // Contar vocales, consonantes y espacios en una oración
+
+#include <stdio.h>
+
+int main()
+{
+    char frase[100];
+    printf("Ingresa una frase: ");
+    scanf("%[^\n]", frase);
+
+    int vocales = 0, consonantes = 0, espacios = 0;
+
+    for (int i = 0; frase[i] != '\0'; i++)
+    {
+        char c = frase[i];
+
+        if (c == ' ')
+        {
+            espacios++;
+        }
+        else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        {
+            if ((c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') || (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'))
+            {
+                vocales++;
+            }
+            else
+            {
+                consonantes++;
+            }
+        }
+    }
+    printf("\nLa cantidad de espacios es: %d", espacios);
+    printf("\nLa cantidad de vocales son: %d", vocales);
+    printf("\nLa cantidad de consonantes es: %d", consonantes);
+
+    return 0;
+}
